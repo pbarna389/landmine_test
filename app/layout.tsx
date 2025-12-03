8edd3601974b9e9c7344522dc5c02370ad21101c
PB <pbarna389@gmail.com>
2025-12-03 14:54:05 +0100

    02/components-header - added base component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 
+import Header from '@/components/Header/Header'
+
 import './globals.css'
 
 const geistSans = Geist({
@@ -35,6 +37,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Header />
 				{children}
 			</body>
 		</html>
