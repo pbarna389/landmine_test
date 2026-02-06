cf64374ef36fd7f5a23d4cddd355b8bff127ee57
PB <pbarna389@gmail.com>
2026-02-06 15:37:02 +0100

    03/component-carousel - added main carousel logic, data structure and animation remains

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,11 +1,37 @@
+'use client'
+
 import Image from 'next/image'
 
-import CarouselImage from './Assets/mm01.webp'
+import { useEffect, useRef, useState } from 'react'
+
+import { CarouselData, CarouselTimeout } from './constants/constants'
 
 export default function Carousel() {
+	const [idx, setIdx] = useState<number>(0)
+
+	const selectedData = CarouselData[idx]
+
+	const intervalTimeout = useRef<ReturnType<typeof setInterval> | null>(null)
+
+	useEffect(() => {
+		intervalTimeout.current = setInterval(() => {
+			setIdx((prev) => (prev + 1) % CarouselData.length)
+		}, CarouselTimeout)
+
+		return () => {
+			if (intervalTimeout.current) {
+				clearInterval(intervalTimeout.current)
+			}
+		}
+	}, [])
+
 	return (
-		<div className="w-full">
-			<Image className="object-center" src={CarouselImage} alt="Carousel Image 1" />
+		<div className="w-full h-screen">
+			<Image
+				className="object-cover h-full w-full overflow-clip"
+				src={selectedData.img}
+				alt="Carousel Image 1"
+			/>
 		</div>
 	)
 }
---
+++ components/Carousel/constants/constants.ts
@@ -0,0 +1,20 @@
+import Image1 from '../assets/mm01.webp'
+import Image2 from '../assets/mm02.webp'
+import Image3 from '../assets/mm03.webp'
+
+export const CarouselData = [
+	{
+		id: 1,
+		img: Image1
+	},
+	{
+		id: 2,
+		img: Image2
+	},
+	{
+		id: 3,
+		img: Image3
+	}
+]
+
+export const CarouselTimeout = 8000
