5280ba92eb52ada53c37dde9849ec286c07f464a
PB <pbarna389@gmail.com>
2026-02-07 01:19:14 +0100

    03/component-carousel - done

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -37,10 +37,12 @@ export default function Carousel() {
 				src={selectedData.img}
 				key={`carousel-image-${selectedData.img}`}
 				alt={`Carousel Image ${idx}`}
-				onLoad={() => requestAnimationFrame(() => setLoaded(true))}
+				onLoad={() => setLoaded(true)}
 				onError={() => setLoaded(true)}
 			/>
-			<div className="absolute z-5 top-3/8 left-1/2 space-y-10 text-center gap-15 transform -translate-1/2 md:translate-0 md:text-left md:top-[29.6%] md:left-1/15 text-5xl font-montserrat font-bold leading-13 transition-all">
+			<div
+				className={`absolute z-5 space-y-10 text-center gap-15 transform -translate-1/2 md:translate-0 md:text-left text-5xl font-montserrat font-bold leading-13 transition-all duration-1000 ${loaded ? 'opacity-100 top-3/8 left-1/2 md:top-[29.6%] md:left-1/15' : 'opacity-0 top-1/2 left-1/2 md:top-[29.6%] md:left-1/2'}`}
+			>
 				<div>
 					<h2 className="font-white">{selectedData.text1[0]}</h2>
 					<h3 className="text-carousel">{selectedData.text1[1]}</h3>
