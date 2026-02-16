790ab5427a667bbacce3ee45d2d59f4925e8005b
PB <pbarna389@gmail.com>
2026-02-16 16:45:31 +0100

    06/premiseSection - added assets and text

---
+++ app/components/PremiseSection/constants/constants.ts
@@ -0,0 +1,23 @@
+export const PREMISE_CONTENT = {
+	header: {
+		title: 'amire számíthatsz',
+		subTitle: 'tapasztalat és rugalmasság'
+	},
+	content: [
+		{
+			title: 'tapasztalat',
+			text:
+				'Immár 20 éves intenzív tanítási tapasztalattal, ill. Bp. egyik legkedvezőbb tandíjával várlak, jó hangulatú, minőségi órákra. Dolgozzunk együtt, valósítsuk meg közösen a céljaidat!'
+		},
+		{
+			title: 'rugalmasság',
+			text:
+				'Gitároktatás kezdőknek, haladóknak - jelentkezés folyamatosan. Tanulj személyre szabott, gyakorlat-központú órákon! Nálam bármilyen stílusban tanulhatsz.'
+		},
+		{
+			title: 'személyes vagy online',
+			text:
+				'Karanténos időkben, ill. a Budapesttől távol élőknek sem kell lemondani a gitározásról! A tanulás online videochat formában is jól működik, facebook-messenger vagy skype programokkal.'
+		}
+	]
+}
