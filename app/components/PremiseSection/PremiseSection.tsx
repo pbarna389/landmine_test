4263e14dcaa34aa80c2959435e5146e3f092f537
PB <pbarna389@gmail.com>
2026-02-16 16:38:36 +0100

    06/premiseSection - init

---
+++ app/components/PremiseSection/PremiseSection.tsx
@@ -0,0 +1,3 @@
+export const PremiseSection = () => {
+	return <div>PremiseSection</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { IntroSection } from './components/IntroSection/IntroSection'
+import { PremiseSection } from './components/PremiseSection/PremiseSection'
 
 export default function Home() {
 	return (
 		<main className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black">
 			<IntroSection />
+			<PremiseSection />
 		</main>
 	)
 }
