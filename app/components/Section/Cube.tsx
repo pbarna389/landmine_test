ad68d80d4aff8b0dc5f8014175889b1c12703d2e
PB <pbarna389@gmail.com>
2026-02-17 17:36:00 +0100

    06/premiseSection - added the sectionHeader cube detail, cards and main stuff remains

---
+++ app/components/Section/Cube.tsx
@@ -0,0 +1,12 @@
+type CubeProps = {
+	className?: string
+	color?: string
+}
+
+export const Cube = ({ className = '', color = 'white' }: CubeProps) => {
+	return (
+		<div
+			className={`w-7 h-7 md:w-12 md:h-12 lg:w-17 lg:h-17 transform -rotate-45 bg-${color} ${className}`}
+		/>
+	)
+}
--- app/components/Section/SectionHeader.tsx
+++ app/components/Section/SectionHeader.tsx
@@ -1,5 +1,25 @@
 import type { SectionProps } from './types'
 
-export const SectionHeader = ({ children, className }: SectionProps) => {
-	return <div className={`${className} relative`}>{children}</div>
+import { Cube } from './Cube'
+
+type SectionHeaderProps = SectionProps & {
+	side?: 'left' | 'right'
+}
+
+export const SectionHeader = ({
+	children,
+	className,
+	side = 'left'
+}: SectionHeaderProps) => {
+	return (
+		<div className={`${className} relative`}>
+			<div
+				className={`absolute top-0 ${side === 'left' ? 'left-0' : 'right-3 sm:right-3 md:right-5 lg:right-7.25'} transform -translate-1/2 translate-x-2/10 sm:space-y-3 sm:top-15/200 md:space-y-5 md:top-31/240 lg:space-y-7 lg:top-8/45`}
+			>
+				<Cube />
+				<Cube color={'cube-secondary hidden sm:block'} />
+			</div>
+			{children}
+		</div>
+	)
 }
