b38041a2ac511889d1bd5e438231d2821536f3f8
PB <pbarna389@gmail.com>
2026-02-25 02:43:58 +0100

    10/hotfixes-header-desktop - done

--- components/Header/DesktopNavbar.tsx
+++ components/Header/DesktopNavbar.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { IconContext } from 'react-icons'
 
 import { NAV_ELEMENTS, NAV_ICONS_DESKTOP } from './constants'
@@ -7,20 +9,26 @@ import { Icons } from '../Icons/Icons'
 export const DesktopNavbar = () => {
 	return (
 		<>
-			<nav className="hidden lg:flex gap-8.5">
-				<ul className="flex lg:gap-5.25 lg:text-black">
-					{NAV_ELEMENTS.map((el) => (
-						<li
-							className="relative font-montserrat uppercase text-[16.4px] font-bold tracking-[2px] h-full flex items-center text-base transition-all duration-300 cursor-pointer select-none hover:text-hover active:text-hover before:absolute before:content-[''] before:w-full before:scale-x-0 before:origin-right before:h-0.5 before:bottom-0 before:left-0 before:transition-transform before:bg-hover hover:before:w-full hover:before:scale-x-100 hover:before:origin-left active:before:scale-x-100 active:before:w-full active:before:origin-left focus:before:scale-x-100 focus:before:w-full focus:before:origin-left"
-							key={`nav-${el}`}
+			<nav className="flex justify-center items-center gap-8.5">
+				<div className="flex gap-5.25 text-black">
+					{NAV_ELEMENTS.map((link) => (
+						<Link
+							href={link.href}
+							target="_blank"
+							className="relative font-montserrat uppercase text-[16.4px] font-bold tracking-[2px] h-full text-base transition-all duration-300 cursor-pointer select-none hover:text-hover active:text-hover before:absolute before:content-[''] before:w-full before:scale-x-0 before:origin-right before:h-0.5 before:bottom-0 before:left-0 before:transition-transform before:bg-hover hover:before:w-full hover:before:scale-x-100 hover:before:origin-left active:before:scale-x-100 active:before:w-full active:before:origin-left focus:before:scale-x-100 focus:before:w-full focus:before:origin-left"
+							key={`nav-${link.text}`}
 						>
-							{el.text}
-						</li>
+							{link.text}
+						</Link>
 					))}
-				</ul>
-				<div className="flex gap-1.5 items-center *:transition-all *:hover:scale-110 *:hover:brightness-125 *:cursor-pointer">
+				</div>
+				<div className="flex gap-1.5 items-center justify-center *:transition-all *:hover:scale-110 *:hover:brightness-125 *:cursor-pointer">
 					<IconContext value={{ size: '28' }}>
-						<Icons iconVars={NAV_ICONS_DESKTOP} />
+						{NAV_ICONS_DESKTOP.map((icon) => (
+							<Link key={`navbar-desktop-icon-${icon.site}`} href={icon.href} target="_blank">
+								<Icons iconVars={icon.site} />
+							</Link>
+						))}
 					</IconContext>
 				</div>
 			</nav>
--- components/Header/constants/constants.ts
+++ components/Header/constants/constants.ts
@@ -9,10 +9,30 @@ export const NAV_ELEMENTS = [
 	{ text: 'cikkek', href: '' }
 ]
 
-export const NAV_ICONS_DESKTOP: IconNames[] = ['tiktok', 'facebook', 'youtube']
+export const NAV_ICONS_DESKTOP: { href: string; site: IconNames }[] = [
+	{
+		site: 'tiktok',
+		href: 'https://www.tiktok.com/@landminegitar'
+	},
+	{
+		site: 'facebook',
+		href: 'https://www.facebook.com/landminegitar/'
+	},
+	{
+		site: 'youtube',
+		href: 'https://www.youtube.com/c/landmine-gitaroktatas/featured'
+	}
+]
 
-export const NAV_ICONS_MOBILE: IconNames[] = [
+export const NAV_ICONS_MOBILE: { href: string; site: IconNames }[] = [
 	...NAV_ICONS_DESKTOP.reverse(),
-	'message',
-	'telephone'
+	{
+		site: 'message',
+		href:
+			'mailto:info@landmine-gitaroktatas.hu?subject=Gitároktatás -jelentkezés, érdeklődés'
+	},
+	{
+		site: 'telephone',
+		href: 'tel:06307207878'
+	}
 ]
