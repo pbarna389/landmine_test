6f0845414f7a3c1552a3e145aab970a48159c409
PB <pbarna389@gmail.com>
2026-03-05 10:44:34 +0100

    14/routeRename - renamed the already implemented route names, added remaining main routes, updated the constants

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<h1 className="text-2xl">Homepage</h1>
+			<h1 className="text-2xl">Videos</h1>
 		</main>
 	)
 }
--- components/Header/constants/constants.ts
+++ components/Header/constants/constants.ts
@@ -3,14 +3,14 @@ import type { LinkWithIconsType } from '@/types'
 
 export const NAV_ELEMENTS = [
 	{ text: 'főoldal', href: '/' },
-	{ text: 'adatlap', href: '/adatlap' },
+	{ text: 'adatlap', href: '/about' },
 	{
 		text: 'gitároktatás',
-		href: './gitaroktatas'
+		href: '/classes'
 	},
-	{ text: 'videók', href: '/' },
-	{ text: 'zenék', href: '/' },
-	{ text: 'cikkek', href: '/' }
+	{ text: 'videók', href: '/videos' },
+	{ text: 'zenék', href: '/music' },
+	{ text: 'cikkek', href: '/articles' }
 ]
 
 export const NAV_ICONS_MOBILE: LinkWithIconsType = [
