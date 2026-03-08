d07a70bf9570444807c1c74c73dfc926a350ceb6
PB <pbarna389@gmail.com>
2026-03-08 16:32:59 +0100

    15-videos-subPages - added constant with text and links

---
+++ app/videos/[videoCategory]/constants/constants.ts
@@ -0,0 +1,87 @@
+export const VIDEO_CATEGORY_CONTENT = {
+	vlog: {
+		title: {
+			text: 'gitáros videók',
+			subText:
+				'Folyamatosan bővülő videós anyagok, többféle kategóriában - témakörökre bontva. \nNe maradj le a legújabb részekről, iratkozz fel a Youtube-on! ;)'
+		},
+		content: [
+			{
+				id: 'interestingVideos',
+				title: 'zenei csemegék',
+				text: [
+					'Gitáros/zenei videók jó zenékkel, jó zenékről, sok gitározással...',
+					'Egy kicsit szakmai, egy kicsit személyes...; érdekes, hasznos, vagy épp meglepő témákkal...',
+					'Folyamatosan bővülő részekkel...'
+				],
+				link: ['https://www.youtube.com/playlist?list=PLn5cNXlh5JYNmWSHVLMsA6F_3wNRhaofk']
+			},
+			{
+				id: 'sologuitar',
+				title: 'szólógitáros technikák',
+				text: [
+					'Menő szólógitár-technikákkal ismerkedhetsz ebben a sorozatban',
+					'Gyakorlási tippek, fincsi figurák, gyakori hibák, példák és szuper gyakorlatok',
+					'Tabokkal, zenei alapokkal - kezdőknek is!'
+				],
+				link: ['https://www.youtube.com/playlist?list=PLn5cNXlh5JYNJwmXN08v2fKEoC7dwTKWv']
+			},
+			{
+				id: 'oneMinutes',
+				title: 'gitáros egypercesek',
+				text: [
+					'Gitártechnikai és gyakorlási tippek (kék videók)',
+					'Zeneelméleti apróságok és ötletelések (piros videók)',
+					'Elemzések, érdekességek, gitársound, stb.. (sárga videók)'
+				],
+				link: ['https://www.youtube.com/playlist?list=PLn5cNXlh5JYPHRBJL_zEBp0TZ7bktfo6Y']
+			},
+			{
+				id: 'creativeRiffs',
+				title: 'kreatív lickek, riffek és akkordmenetek',
+				text: [
+					'A jó lickek szélesíthetik látókörünket, meglökhetik kreativitásunkat.. Jól esik az agynak, jól esik a kéznek!',
+					'Az ötletes riffek és akkordkörök inspirációt nyújthatnak -és még technikailag is fejlesztenek.',
+					'Szuper hazai, külföldi és saját témákból...'
+				],
+				link: [
+					'https://www.youtube.com/playlist?list=PLn5cNXlh5JYOkBpxQwINbPpTYBdFwn_xD',
+					'https://www.youtube.com/playlist?list=PLn5cNXlh5JYP7tqC2WmM6yB86XyK1Crwb',
+					'https://www.youtube.com/playlist?list=PLn5cNXlh5JYMDfijriMMmsFTR2MQPFTp0'
+				]
+			}
+		]
+	},
+	theory: {
+		title: {
+			text: 'Zeneelmélet és hallás',
+			subText:
+				'Az alábbi lapon folyamatosan bővülő zeneelméleti anyagokat találsz. \nNe maradj le a legújabb részekről, iratkozz fel a Youtube-on! ;)'
+		},
+		content: [
+			{
+				id: 'theoryVideos',
+				title: 'Zeneelméleti videók',
+				text: [
+					'Zeneelméleti látókör-szélesítés, ötletelések nem csak gitárosoknak.',
+					'Ha érdekel a háttér, a "hogyanok és miértek", ne hagyd ki!',
+					'Folyamatosan bővülő videók az önálló és kreatív gyakorláshoz'
+				],
+				link: ['https://www.youtube.com/playlist?list=PLn5cNXlh5JYOK0qIXqaZz0LsQ0zHpK30U']
+			},
+			{
+				id: 'improvisation',
+				title: 'zeneírás és improvizálás',
+				text: [
+					'Improvizációs tippek és témák nem csak gitárosoknak.',
+					'Zeneírós kulisszatitkok, inspiráció, hangszerelés, otthoni felvételkészítés.',
+					'Zenei háttérmunkák az alapötlettől a végkifejletig.'
+				],
+				link: [
+					'https://www.youtube.com/playlist?list=PLn5cNXlh5JYP_qrnVQGcRtylKSUVF5AOU',
+					'https://www.youtube.com/playlist?list=PLn5cNXlh5JYMEgspuw_Ns5tqzHOF0zCro'
+				]
+			}
+		]
+	}
+}
